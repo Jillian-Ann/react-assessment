261e4b829619d33f3a004d844562f4aa5353eb7a
Jay <jawan94@gmail.com>
2019-12-10 20:07:30 -0500

    segment rendering to functions

--- src/BookList.js
+++ src/BookList.js
@@ -5,68 +5,98 @@ class BookList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            results: [
-                  {
-                      "title": "A COLUMN OF FIRE"
-                  },
-                  {
-                      "title": "A Dangerous Fortune"
-                  },
-                  {
-                      "title": "Code to Zero"
-                  },
-                  {
-                      "title": "EDGE OF ETERNITY"
-                  },
-                  {
-                      "title": "EYE OF THE NEEDLE"
-                  },
-                  {
-                      "title": "FALL OF GIANTS"
-                  },
-                  {
-                      "title": "FALL OF GIANTS"
-                  },
-                  {
-                      "title": "Lie Down With Lions"
-                  },
-                  {
-                      "title": "On Wings of Eagles"
-                  },
-                  {
-                      "title": "PILLARS OF THE EARTH"
-                  }
-              ],
+            results: [],
               inputValue: ""
         };
     }
 
     onInputChange(e){
-      let temp = this.state.results.slice(0);
       this.setState({
         inputValue: e.target.value,
-        results: temp.push({"title": e.target.value})
       })
-      console.log(this.state.results);
     }
+
+    addBookTitle(){
+      let results = this.state.results
+      results.push({"title": this.state.inputValue})
+
+      this.setState({
+        results,
+        inputValue: ""
+      })
+    };
+
+    listItems() {
+    let items = this.state.results;
+    return (
+      <ul>
+        {
+          items.map((val, index) => {
+            return (
+              <li key={index}>
+                { val.title }
+              </li>
+            );
+          })
+        }
+      </ul>
+    );
+  }
+
+  componentWillMount(){
+    this.setState({
+        results: [
+              {
+                  "title": "A COLUMN OF FIRE"
+              },
+              {
+                  "title": "A Dangerous Fortune"
+              },
+              {
+                  "title": "Code to Zero"
+              },
+              {
+                  "title": "EDGE OF ETERNITY"
+              },
+              {
+                  "title": "EYE OF THE NEEDLE"
+              },
+              {
+                  "title": "FALL OF GIANTS"
+              },
+              {
+                  "title": "FALL OF GIANTS"
+              },
+              {
+                  "title": "Lie Down With Lions"
+              },
+              {
+                  "title": "On Wings of Eagles"
+              },
+              {
+                  "title": "PILLARS OF THE EARTH"
+              }
+          ],
+          inputValue: ""
+  })
+}
 
 
     render() {
         return (
             <div className="bookListMain">
                 <div className="header">
+
                 <form>
                     <input onChange ={ (e) => this.onInputChange(e) }placeholder="Book" />
-                    <button>
+                    <button onClick = {() => this.addBookTitle() }>
                         Add Book
                     </button>
                     <button> Get Top 10 Books by Ken Follet</button>
                 </form>
-                <ul>
-                {this.state.results.map(items => (
-                  <li key = {items.title}>{items.title}</li>
-                ))}
-                </ul>
+
+                { this.listItems() }
+
                 </div>
             </div>
         )
